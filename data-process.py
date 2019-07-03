248351d2afad0c4832622ba6c77ac47bf1af13f1
aakashpaliwal08 <52446425+aakashpaliwal08@users.noreply.github.com>
2019-07-03 16:50:13 +0530

    Create data-process.py

---
+++ data-process.py
@@ -0,0 +1,28 @@
+import config 
+import os
+
+ lines=open('movie_lines.txt').read().split('\n')
+def get_line() :
+  id_idvalueline = {}
+  for i in lines:
+        new_line = line.split(' +++$+++ ')
+          if len(new_line) == 5:
+            id2line[new_line[0]] = new_line[4]
+    return id_idvalueline 
+    
+    
+    
+    
+    
+    
+def get_dir (): 
+
+
+    return 
+    
+    
+
+def get_conversation(): 
+
+
+      return 
