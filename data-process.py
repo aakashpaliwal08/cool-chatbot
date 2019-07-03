76e845d53344075738543611ccdf8f4e2276b468
Rajib Mitra <rajib.jolite@gmail.com>
2019-07-03 15:13:52 +0530

    Create data-process.py

---
+++ data-process.py
@@ -0,0 +1,27 @@
+import config 
+import os
+
+
+def get_line() :
+
+    
+    
+    
+    
+    return 
+    
+    
+def get_dir (): 
+
+
+    return 
+    
+    
+
+def get_conversation(): 
+
+
+      return 
+      
+      
+ 
