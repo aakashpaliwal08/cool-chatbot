748476cbd1ae9aceacfef825deb392dd964ce286
aakashpaliwal08 <52446425+aakashpaliwal08@users.noreply.github.com>
2019-07-03 16:39:45 +0530

    Update data-process.py

--- data-process.py
+++ data-process.py
@@ -1,14 +1,23 @@
 import config 
 import os
 
-
+lines=open('movie_lines.txt').read().split('\n')
 def get_line() :
+    #creating Dictionariy for storing key and its values
+    get_idvalueline={}
+    for i in lines:
+        new_line=i.split('+++$+++')
+        if len(return new_line)==5:
+            get_idvalueline[new_line[0]]=new_line[4]
+    
+    return get_idvalueline
+    
 
     
     
     
     
-    return 
+   
     
     
 def get_dir (): 
